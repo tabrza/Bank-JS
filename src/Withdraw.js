52c530d01b85f4e68a98253a4f962c263fb36ded
Tabish Raza <tabishraza@gmail.com>
2018-01-02 16:01:36 +0000

    every withdraw has a date

--- src/Withdraw.js
+++ src/Withdraw.js
@@ -1,6 +1,7 @@
 (function(exports){
-    function Withdraw(amount){
+    function Withdraw(amount, date = Date.now()){
         this.balance = amount;
+        this.date = date;
     }
 
     exports.Withdraw = Withdraw;
