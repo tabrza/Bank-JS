66c5b17d6ffb6147d2c28df6d0a607d9477e9517
Tabish Raza <tabishraza@gmail.com>
2018-01-02 15:19:26 +0000

    test for multiple transactions
    account can deposit and withdraw

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -1,54 +1,21 @@
-// describe("Account", function () {
-//     "use strict";
-//     var account;
-//
-//     beforeEach(function () {
-//         account = new Account();
-//     });
-//
-//     it("initializes with a value of 0", function () {
-//         expect(account.balance).toEqual(0);
-//     });
-//
-//     describe("deposit", function () {
-//         beforeEach(function () {
-//             account.deposit(10);
-//         });
-//
-//         it("returns a value of 10", function () {
-//             expect(account.balance).toEqual(10);
-//         });
-//     });
-//
-//     describe("withdraw", function () {
-//         beforeEach(function () {
-//             account.deposit(10);
-//         });
-//
-//         it("returns a value of 5", function () {
-//             account.withdraw(5);
-//             expect(account.balance).toEqual(5);
-//         });
-//
-//         it("cannot withdraw more than balance", function () {
-//           expect(function () {
-//             expect(account.withdraw(15));
-//           }).toThrowError("Insufficient funds");
-//         });
-//     });
-//
-//     describe("account history", function () {
-//         beforeEach(function () {
-//           var date = moment('2018-01-02').toDate();
-//           jasmine.clock().mockDate(date);
-//         });
-//
-//         it("sets a date deposit", function () {
-//             account.deposit(10);
-//             expect('2018-01-02').toEqual(account.history);
-//         });
-//     });
-//
-//
-//
-// });
+describe("Account", function () {
+    var account;
+
+    beforeEach(function (){
+      account = new Account();
+    });
+
+    describe("#deposit", function () {
+        it("adds a new deposit", function(){
+            account.deposit(10);
+            expect(account.transactions[0].balance).toEqual(10);
+        });
+    });
+
+    describe("#withdraw", function(){
+        it("adds a new withdraw", function(){
+            account.withdraw(5);
+            expect(account.transactions[0].balance).toEqual(5);
+        });
+    });
+});
--- src/Account.js
+++ src/Account.js
@@ -1,20 +1,17 @@
-function Account() {
-    "use strict";
-    this.balance = 0;
-    this.history = null;
-}
-
-// 
-// Account.prototype.deposit = function (amount) {
-//     "use strict";
-//     this.balance += amount;
-// };
-
-Account.prototype.withdraw = function (amount) {
-    "use strict";
-    if (amount > this.balance) {
-        throw new Error("Insufficient funds");
+(function(exports){
+    function Account(){
+      this.transactions = [];
     }
-    this.balance -= amount;
-    this.history = moment().format("MMM Do YY");
-};
+
+    Account.prototype.deposit = function(amount){
+      var deposit = new Deposit(amount);
+      this.transactions.push(deposit);
+    };
+
+    Account.prototype.withdraw = function(amount){
+      var withdraw = new Withdraw(amount);
+      this.transactions.push(withdraw);
+    };
+    
+    exports.Account = Account;
+})(this);
