f7cb0c27ba101e1985cf16b40c38d7c61863a0f7
Tabish Raza <tabishraza@gmail.com>
2018-01-02 16:21:38 +0000

    cannot withdraw more than balance

--- src/Account.js
+++ src/Account.js
@@ -1,17 +1,26 @@
 (function(exports){
-    function Account(){
+    function Account(balance = 0){
       this.transactions = [];
+      this.balance = balance;
     }
 
     Account.prototype.deposit = function(amount){
       var deposit = new Deposit(amount);
+      this.balance += amount;
       this.transactions.push(deposit);
     };
 
     Account.prototype.withdraw = function(amount){
+      if(amount > this.balance){
+        throw new Error("Insufficient funds");
+      }
       var withdraw = new Withdraw(amount);
       this.transactions.push(withdraw);
     };
-     
+
+    Account.prototype.allTransactions = function(){
+      return this.transactions;
+    };
+
     exports.Account = Account;
 })(this);
