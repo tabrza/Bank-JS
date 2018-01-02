b249d741818aef5de65e14a080a4a40d90989782
Tabish Raza <tabishraza@gmail.com>
2018-01-02 17:11:02 +0000

    refactor

--- src/Account.js
+++ src/Account.js
@@ -1,25 +1,37 @@
-(function(exports){
-    function Account(balance = 0){
+(function (exports) {
+    "use strict";
+    function Account(balance= 0){
       this.transactions = [];
       this.balance = balance;
     }
 
     Account.prototype.deposit = function(amount){
+      var that = this;
       var deposit = new Deposit(amount);
-      this.balance += amount;
-      this.transactions.push(deposit);
+      addAmount(amount);
+      this.addTransaction(deposit);
+
+      function addAmount(amount){
+        that.balance += amount;
+      }
     };
 
     Account.prototype.withdraw = function(amount){
-      if(amount > this.balance){
+      var checkAmountToBalance = amount > this.balance;
+      if(checkAmountToBalance){
         throw new Error("Insufficient funds");
       }
+
       var withdraw = new Withdraw(amount);
-      this.transactions.push(withdraw);
+      this.addTransaction(withdraw);
     };
 
     Account.prototype.allTransactions = function(){
       return this.transactions;
+    };
+
+    Account.prototype.addTransaction = function(transaction){
+      this.transactions.push(transaction);
     };
 
     exports.Account = Account;
