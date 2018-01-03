1a2f38a8bd795fc36ea747823e52d89979e4b450
Tabish Raza <tabishraza@gmail.com>
2018-01-03 17:41:38 +0000

    beforeEach correction
    needs mocking

--- spec/AccountViewSpec.js
+++ spec/AccountViewSpec.js
@@ -1,11 +1,37 @@
 describe("AccountView", function(){
+  var accountView, deposit, withdraw, account;
 
-  beforeEach( function () {
-    var deposit = new Deposit(20, "credit", 0);
-    var withdraw = new Withdraw(5, "debit", 20);
+  // beforeEach( function () {
+  //   double = {
+  //     deposit: function(amount, type, balance, date) {
+  //       return [amount, type, balance, date];
+  //     },
+  //     withdraw: function(amount, type, balance, date) {
+  //       return [amount, type, balance, date];
+  //     },
+  //     addTransaction: function(value){
+  //       return value;
+  //     },
+  //   };
+  //   deposit = double.deposit(20, "credit", 0, "03/01/2018");
+  //   withdraw = double.withdraw(5, "debit", 20, "03/01/2018");
+  //   first = double.addTransaction(deposit);
+  //   second = double.addTransaction(withdraw);
+  //   console.log(double)
+  //   account = [];
+  //   account.push(first);
+  //   account.push(second);
+  //   console.log(account)
+  //   accountView = new AccountView(account);
+  //   console.log(accountView)
+  // });
+
+  beforeEach ( function () {
     account = new Account();
-    account.addTransaction(deposit);
-    account.addTransaction(withdraw);
+    account.deposit(10);
+    account.withdraw(5);
+    deposit = new Deposit(10, "credit", 0, "03/01/2018");
+    withdraw = new Withdraw(5, "debit", 20, "03/01/2018");
     accountView = new AccountView(account);
   });
 
