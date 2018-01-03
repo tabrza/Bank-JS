f318d53e4388209025f2bb8855ece567a5e9a6ba
Tabish Raza <tabishraza@gmail.com>
2018-01-03 11:27:28 +0000

    test for rendering transactions

--- spec/AccountViewSpec.js
+++ spec/AccountViewSpec.js
@@ -1,13 +1,19 @@
 describe("AccountView", function(){
 
   beforeEach( function () {
-    accountView = new AccountView();
+    var deposit = new Deposit(20, "credit", 0);
+    var withdraw = new Withdraw(5, "debit", 20);
+    account = new Account();
+    account.addTransaction(deposit);
+    account.addTransaction(withdraw);
+    accountView = new AccountView(account);
   });
 
 
-  describe("#print", function(){
-    it("prints account statement", function(){
-
+  describe("#render", function(){
+    it("renders account statement", function(){
+      expect(accountView.render()).toContain('credit');
+      expect(accountView.render()).toContain('debit');
     });
   });
 
