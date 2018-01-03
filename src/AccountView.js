9db697b53f587a2a70e6cd8002e1b12550ff6a48
Tabish Raza <tabishraza@gmail.com>
2018-01-03 11:27:46 +0000

    account renders all transactions

--- src/AccountView.js
+++ src/AccountView.js
@@ -4,6 +4,19 @@
     }
 
 
+    AccountView.prototype.render = function(){
+      list = [];
+      for (var i = 0; i < this.account.transactions.length; i++){
+        list.push(`<li><div>
+                  Date ${this.account.transactions[i].date}
+                  Type ${this.account.transactions[i].type}
+                  Credit ${this.account.transactions[i].credit}
+                  Debit ${this.account.transactions[i].debit}
+                  Balance ${this.account.transactions[i].current_balance}
+                  </div></li>`)
+      }
+      return ("<ul>" + list.join(' ') + "</ul>");
+    };
 
-
+    exports.AccountView = AccountView;
 })(this);
