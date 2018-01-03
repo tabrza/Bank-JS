64515531eeff8505da52ebf0d296622ebc201387
Tabish Raza <tabishraza@gmail.com>
2018-01-03 17:40:34 +0000

    date format correction

--- src/Withdraw.js
+++ src/Withdraw.js
@@ -1,5 +1,5 @@
 (function(exports){
-    function Withdraw(amount, type, balance, date = Date.now()){
+    function Withdraw(amount, type, balance, date = new Date()){
         this.debit = amount;
         this.type = type;
         this.current_balance = balance;
